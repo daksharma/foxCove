fbb078c2b6cdfb0de0a311093c0caa84fca0ee76
Angelina <angelina.may@hotmail.com>
2016-08-24 16:43:39 -0700

    (added the files mentioned) wrote out basic server with Express, JSON package created. -m"[feature]create basic server and package JSON"

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+//db
+var request = require('request')
+var path = require('path');
+var convert = require('x2js');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(express.static(path.join(__dirname, 'client')))
+
+
+app.listen(3000, function(){console.log('server started...')})
+
