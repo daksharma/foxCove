b749f9d01f935fa3cc351e7767b30592db43a1e2
Eric Churchill <eric.churchill2@gmail.com>
2016-08-25 08:29:24 -0700

    [bug out] Add forward slash to static file directory

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-app.use(express.static(path.join(__dirname, 'client')))
 
+app.use(express.static(path.join(__dirname, '/client')))
 
 app.listen(3000, function(){console.log('server started...')})
-
