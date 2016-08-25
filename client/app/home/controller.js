5d317a78d12270a448d782473535d085cc41f171
Daksh Sharma <dashatrix@gmail.com>
2016-08-25 11:53:45 -0700

    [Feature] Angular factory/controller view modificatio and post request

--- client/app/home/controller.js
+++ client/app/home/controller.js
@@ -1,9 +1,25 @@
 angular.module('app.home', [])
 
-.controller('HomeController', function($scope) {
-
+.controller('HomeController', function($scope, userLocationFactory) {
   $scope.submit = function() {
-    alert($scope.location);
-  }
-
+    // alert($scope.location);
+    userLocationFactory.sendZipCode($scope.location);
+  };
 })
+
+.factory('userLocationFactory', function($http){
+  var serverUrl = 'localhost:3000'; // TODO : properly change server/localhost
+  function sendZipCode(userZipCode) {
+    $http.post(serverUrl + '/getreps', userZipCode)
+         .then(function (data, error) {
+           console.log(data);
+           if (error) {
+             console.log(error);
+           }
+           return 'hello';
+         });
+  }
+  return {
+    sendZipCode : sendZipCode
+  }
+});
