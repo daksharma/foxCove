4dc38c804d51ad13151221a3e715940c75d043a9
Eric Churchill <eric.churchill2@gmail.com>
2016-08-26 16:10:32 -0700

    [bug out] Getbills module stable

--- server/modules/bill-summary.js
+++ server/modules/bill-summary.js
@@ -5,7 +5,7 @@ var key = require('../secret/api-keys');
 // Congressional API
 
 module.exports = function(bill_id, callback) {
-  
+
   var httpRequestOptions = {
     url: 'https://congress.api.sunlightfoundation.com/bills?bill_id=' + bill_id,
     headers: {
