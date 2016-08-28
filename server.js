73d590976c85e56a618b68d7f60163bad5c3dd90
Casey Childers <cachilders@gmail.com>
2016-08-28 09:39:22 -0700

    [bug out] Add crash handling to zipcode lookup

--- server.js
+++ server.js
@@ -37,32 +37,37 @@ app.post('/getReps', function(req, res){
     console.log(req.body.zipcode);
     var zip = req.body.zipcode; //front end request should be in the format {zipcode: zipcode}
     request('https://congress.api.sunlightfoundation.com/legislators/locate?zip=' + zip + '&apikey=fca53d5418a64a6a81b29bb71c97b9a1', function(error, response, data){
-        data = JSON.parse(data);
-        var obj = {};
-        obj.reps = [];
-        for(var i = 0; i < data.results.length; i++){
-            var package = {};
-            var person = data.results[i];
-            package.bioguide_id = person.bioguide_id;
-            package.name = person.first_name + " " + person.last_name;
-            package.title = person.title === "Sen" ?  "Senator" : "Representative";
-            package.district = person.district;
-            package.email = person.oc_email;
-            package.twitter = person.twitter_id;
-            package.website = person.website;
-            if(person.party === "R"){
-                package.affiliation = "Republican";
+        if (!data.includes('<')) {
+            data = JSON.parse(data);
+            var obj = {};
+            obj.reps = [];
+            for(var i = 0; i < data.results.length; i++){
+                var package = {};
+                var person = data.results[i];
+                package.bioguide_id = person.bioguide_id;
+                package.name = person.first_name + " " + person.last_name;
+                package.title = person.title === "Sen" ?  "Senator" : "Representative";
+                package.district = person.district;
+                package.email = person.oc_email;
+                package.twitter = person.twitter_id;
+                package.website = person.website;
+                if(person.party === "R"){
+                    package.affiliation = "Republican";
+                }
+                else if (person.party === "D"){
+                    package.affiliation = "Democrat";
+                }
+                else{
+                    package.affiliation = "Independent";
+                }
+                obj.reps.push(package);
             }
-            else if (person.party === "D"){
-                package.affiliation = "Democrat";
-            }
-            else{
-                package.affiliation = "Independent";
-            }
-            obj.reps.push(package);
+            console.log(obj);
+            res.send(obj);
+        } else {
+            console.log('There was a problem with the data provider.');
+            res.sendStatus(500);
         }
-        console.log(obj);
-        res.send(obj);
     });
 });
 
