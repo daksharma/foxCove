019d4802210d858e91d7a369bc6ec05209553c86
Casey Childers <cachilders@gmail.com>
2016-08-29 16:30:31 -0700

    [feature] Add URL params for bookmark/breadcrumb

--- client/app/home/controller.js
+++ client/app/home/controller.js
@@ -3,25 +3,20 @@ angular.module('app.home', [])
 .controller('HomeController', ['$scope','Location', '$state', function($scope, Location, $state) {
   $scope.loadZip = function() {
     if ($scope.location) {
-      $scope.submit();
+      Location.getRepFromZip($scope.location)
+        .then(function(results){
+          $scope.reps = results.reps;
+          $scope.reps.forEach(function(rep) {
+            rep.thumb = 'http://theunitedstates.io/images/congress/225x275/' + rep.bioguide_id + '.jpg';
+          });
+        });
     }
   }
   $scope.submit = function() {
-    Location.getRepFromZip($scope.location)
-      .then(function(results){
-        $scope.reps = results.reps;
-        $scope.reps.forEach(function(rep) {
-          rep.thumb = 'http://theunitedstates.io/images/congress/225x275/' + rep.bioguide_id + '.jpg';
-          rep.profile = 'repProfile({ bioguide_id:' + rep.bioguide_id + '})';
-        });
-      })
-      .then(function() {
-        // non-functioning. Should update URL on successful search
-        // $state.transitionTo($state.searchZip, $scope.location);
-      });
+    console.log($scope.location)
+    $state.go('searchZip', {zipcode: $scope.location})
   }
   $scope.loadProfile = function (rep) {
-    console.log($scope)
     $state.go('repProfile', {bioguide_id: rep.bioguide_id});
   }
 }])
