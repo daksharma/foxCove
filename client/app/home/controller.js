59ad94a7620afeaaa0c787ba32f65f52d4cfdaf8
Casey Childers <cachilders@gmail.com>
2016-08-29 15:59:25 -0700

    [feature] Implement rep profile view and parsing

--- client/app/home/controller.js
+++ client/app/home/controller.js
@@ -1,6 +1,6 @@
 angular.module('app.home', [])
 
-.controller('HomeController', ['$scope','Location', '$state', '$stateParams', function($scope, Location, $state, $stateParams) {
+.controller('HomeController', ['$scope','Location', '$state', function($scope, Location, $state) {
   $scope.loadZip = function() {
     if ($scope.location) {
       $scope.submit();
@@ -11,13 +11,28 @@ angular.module('app.home', [])
       .then(function(results){
         $scope.reps = results.reps;
         $scope.reps.forEach(function(rep) {
-          rep.img = 'http://theunitedstates.io/images/congress/450x550/' + rep.bioguide_id + '.jpg';
           rep.thumb = 'http://theunitedstates.io/images/congress/225x275/' + rep.bioguide_id + '.jpg';
+          rep.profile = 'repProfile({ bioguide_id:' + rep.bioguide_id + '})';
         });
       })
       .then(function() {
         // non-functioning. Should update URL on successful search
         // $state.transitionTo($state.searchZip, $scope.location);
       });
+  }
+  $scope.loadProfile = function (rep) {
+    console.log($scope)
+    $state.go('repProfile', {bioguide_id: rep.bioguide_id});
+  }
+}])
+
+.controller('ProfileController', ['$scope','RepProfile', '$state', function($scope, RepProfile, $state) {
+  $scope.build = function() {
+    RepProfile.getRepFromBioId($state.params)
+      .then(function(results){
+        $scope.rep = results.rep;
+        console.log($scope.rep.bioguide_id)
+        $scope.rep.img = 'http://theunitedstates.io/images/congress/450x550/' + $scope.rep.bioguide_id + '.jpg';
+      })
   };
-}]);
+}])
