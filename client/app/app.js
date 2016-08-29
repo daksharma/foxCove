3a325b594c5f1b9b489b7602918a294bc15b78c7
Casey Childers <cachilders@gmail.com>
2016-08-29 13:27:42 -0700

    [feature] Add zip search by URL param

--- client/app/app.js
+++ client/app/app.js
@@ -15,10 +15,12 @@ angular.module('foxCove', [
     url: '/',
     controller: 'HomeController'
   })
-  .state('search', {
-    templateUrl: 'app/search/search.html',
-    url: '/search',
-    controller: 'SearchResultController'
+  .state('searchZip', {
+    templateUrl: 'app/home/view.html',
+    url: '/:zipcode',
+    controller: function($scope, $stateParams) {
+      $scope.location = $stateParams.zipcode;
+    }
   })
   // .state('profile',{
   //   templateUrl: 'app/profile/rep-profile-view.html',
--- client/app/home/controller.js
+++ client/app/home/controller.js
@@ -1,6 +1,11 @@
 angular.module('app.home', [])
 
-.controller('HomeController', ['$scope','Location', function($scope, Location) {
+.controller('HomeController', ['$scope','Location', '$state', '$stateParams', function($scope, Location, $state, $stateParams) {
+  $scope.loadZip = function() {
+    if ($scope.location) {
+      $scope.submit();
+    }
+  }
   $scope.submit = function() {
     Location.getRepFromZip($scope.location)
       .then(function(results){
@@ -9,6 +14,10 @@ angular.module('app.home', [])
           rep.img = 'http://theunitedstates.io/images/congress/450x550/' + rep.bioguide_id + '.jpg';
           rep.thumb = 'http://theunitedstates.io/images/congress/225x275/' + rep.bioguide_id + '.jpg';
         });
+      })
+      .then(function() {
+        // non-functioning. Should update URL on successful search
+        // $state.transitionTo($state.searchZip, $scope.location);
       });
   };
 }]);
