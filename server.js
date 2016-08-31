992c5e63661cdf9f1fb85bbd3133ba009b81dd1f
Casey Childers <cachilders@gmail.com>
2016-08-31 12:10:46 -0700

    [bug-out] Restore lost https require

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express = require('express');
 require('dotenv').config();
+var request = require('request');
+var path = require('path');
+var convert = require('x2js');
+var favicon = require('serve-favicon');
+var govTrack = require('govtrack-node');
+var https = require('https');
+var civicInfo = require('civic-info')({apiKey: 'AIzaSyC-vnNvHhV7SzFMEA2mXaP3Eo05RakGXqA'}); // <-- ¯\_(ツ)_/¯
 var mongoDb = require('./db/mdb-config');
 var bookshelf = require('./db/pg-db-config');
 var models = require('./db/pg-models');
 var collections = require('./db/pg-collections');
-var request = require('request');
-var path = require('path');
-var convert = require('x2js');
 var bodyParser = require('body-parser');
 var sponsorship = require('./server/modules/sponsorship-history');
 var newsfeed = require('./server/modules/news-feed');
 var info = require('./server/modules/basic-info')
-var favicon = require('serve-favicon');
-var govTrack = require('govtrack-node');
-var civicInfo = require('civic-info')({apiKey: 'AIzaSyC-vnNvHhV7SzFMEA2mXaP3Eo05RakGXqA'});
 var localReps = require('./server/modules/local-officials');
 
 
