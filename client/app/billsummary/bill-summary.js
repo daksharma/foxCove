b9bc738aeeaca335bec45d1a1675484519527638
Daksh Sharma <dashatrix@gmail.com>
2016-08-31 19:01:44 -0700

    [STYLE] fix style for bill summary controller

--- client/app/billsummary/bill-summary.js
+++ client/app/billsummary/bill-summary.js
@@ -1,18 +1,17 @@
 angular.module('app.bill', [])
-       .controller('BillSummary', ['$scope', 'GetBillSummary', '$state', function ($scope, GetBillSummary, $state) {
-
-         $scope.getBillSum = function () {
-           // for now get something back from server for one specific bill
-           GetBillSummary.getBillSummary(71410)
-                         .then(function(data){
-                           // don't worry if these dot variables are complaining
-                           $scope.billTitle = data.title;
-                           $scope.introDate = data.introduced_date;
-                           $scope.curBillStatus = data.current_status_description;
-                           $scope.officialIntro = data.titles[1][2];
-                         });
-         }
-       }]);
+.controller('BillSummary', ['$scope', 'GetBillSummary', '$state', function ($scope, GetBillSummary, $state) {
+  $scope.getBillSum = function () {
+    // for now get something back from server for one specific bill
+    GetBillSummary.getBillSummary(71410)
+                  .then(function(data){
+                    // don't worry if these dot variables are complaining
+                    $scope.billTitle = data.title;
+                    $scope.introDate = data.introduced_date;
+                    $scope.curBillStatus = data.current_status_description;
+                    $scope.officialIntro = data.titles[1][2];
+                  });
+  }
+}]);
 
 
 
