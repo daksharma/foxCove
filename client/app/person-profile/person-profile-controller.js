64a946ff0c1d4937a090c2f4e5ebc2989c87727f
Casey Childers <cachilders@gmail.com>
2016-09-05 09:12:38 -0700

    [bug out] Repair merge-broken financial details

--- client/app/person-profile/person-profile-controller.js
+++ client/app/person-profile/person-profile-controller.js
@@ -3,15 +3,23 @@ angular.module('app.personProfile',[])
 .controller('ProfileController', ['$scope','RepProfile', 'RepBio','$state', 'Affiliations', 'RepBills', function($scope, RepProfile, RepBio, $state, Affiliations, RepBills) {
 
   $scope.build = function() {
-    RepProfile.getRepFromBioId($state.params)
+    var person = $state.params;
+    console.log(person)
+    if (person.leg_id) {
+      console.log('STATE REP: ', person);
+    } else if (person.bioguide_id) {
+    RepProfile.getRepFromBioId(person)
       .then(function(results){
         RepProfile.repObject = results;
         $scope.rep = results.rep;
         $scope.rep.img = 'http://theunitedstates.io/images/congress/450x550/' + $scope.rep.bioguide_id + '.jpg';
         $scope.getBio($scope.rep);
-        $scope.getAffiliation($scope.rep)
+        $scope.getAffiliation($scope.rep);
         $scope.getBills($scope.rep);
       })
+    } else {
+      $scope.nope(personId);
+    }
   };
 
   $scope.getBio = function(rep) {
@@ -20,22 +28,29 @@ angular.module('app.personProfile',[])
         rep.bio = results.split('\n')[0] || 'No biographical information available at this time for ' + rep.firstname + ' ' + rep.lastname;
       })
   }
+
   $scope.getAffiliation = function(rep) {
     Affiliations.getAffiliations(rep)
       .then(function(results) {
         $scope.rep.affiliations = results;
+        $scope.format = Affiliations.formatCurrency
       })
 
   }
+
   $scope.getBills = function(rep) {
     RepBills.getBillsFromRepId({bioguideId: rep.bioguide_id})
       .then(function(results) {
         $scope.bills = results.results;
       });
   }
+
   $scope.loadBill = function (bill) {
     $state.go('billSummary', bill);
   }
 
-  // $scope.format = Affiliations.formatCurrency
+  $scope.nope = function(str) {
+    $state.go('errorResponse', {str: str});
+  }
+
 }]);
