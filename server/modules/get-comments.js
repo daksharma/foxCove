9338f103359bd71798c01217ee8fa8aefdef8190
Angelina <angelina.may@hotmail.com>
2016-09-06 15:12:55 -0700

    [feature]create basic comments format

--- server/modules/get-comments.js
+++ server/modules/get-comments.js
@@ -1,12 +1,12 @@
 
 module.exports = function(req, res) {
   res.send({comments: [{
-    content: "hello",
+    content: "ladyfox checking in",
     username: "ladyfox",
     time:"9/6/2016 14:48:10"
   },
   {
-    content: "hi",
+    content: "Nancy Pelosi is awesome!",
     username: "ladyfox",
     time:"9/6/2016 14:55:10"
   }
