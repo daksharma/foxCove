4afd1ba17efc61822c8c35242818221f7e7e4621
Eric Churchill <eric.churchill2@gmail.com>
2016-09-07 02:20:47 -0700

    [style] Lint all files

--- client/app/d3/d3.js
+++ client/app/d3/d3.js
@@ -15,6 +15,7 @@ angular.module('d3directive', [])
         // ASSIGN colors to chart paths
         var color = d3.scaleOrdinal()
           .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
         var pie = d3.pie()
           .sort(null)
           .value(function(d) {
